9cb732c2b66e3576d6888e72265f726e6c283716
Erik Larkö <erik@larko.se>
2016-03-02 08:48:17 +0100

    Updated readme and fixed problems where souts in paddle code could break things

--- Headless/src/main/java/se/purplescout/pong/competition/headless/fightinotherjvm/ClassThatExecutesFightInOtherJvm.java
+++ Headless/src/main/java/se/purplescout/pong/competition/headless/fightinotherjvm/ClassThatExecutesFightInOtherJvm.java
@@ -7,22 +7,22 @@ import se.purplescout.pong.competition.paddlecache.PaddleCache;
 import se.purplescout.pong.competition.paddlecache.RegisterTimeoutException;
 import se.purplescout.pong.game.Paddle;
 
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.PrintStream;
 
 class ClassThatExecutesFightInOtherJvm {
 
     public static void main(String[] args) throws ClassNotFoundException, RegisterTimeoutException, GetTeamNameException, NewInstanceException, IOException {
+        PrintStream oldStdOut = System.out;
+        PrintStream oldStdErr = System.err;
+        redirectStdStreams();
+
         String leftPaddleClassName = args[0];
         String rightPaddleClassName = args[1];
 
-        Class<Paddle> leftPaddle = (Class<Paddle>) Class.forName(leftPaddleClassName);
-        Class<Paddle> rightPaddle = (Class<Paddle>) Class.forName(rightPaddleClassName);
-
-        PaddleCache.registerNewPaddle(leftPaddle);
-        PaddleCache.registerNewPaddle(rightPaddle);
-
-        Paddle left = PaddleCache.getInstance(leftPaddle);
-        Paddle right = PaddleCache.getInstance(rightPaddle);
+        Paddle left = loadPaddle(leftPaddleClassName);
+        Paddle right = loadPaddle(rightPaddleClassName);
         if (left == null || right == null) {
             System.err.println("Some fight pair contained nulls :/");
             System.exit(1);
@@ -31,9 +31,28 @@ class ClassThatExecutesFightInOtherJvm {
         AutoFight autoFight = new AutoFight(left, right);
         autoFight.run();
 
+        // Restore the standard streams again
+        System.setOut(oldStdOut);
+        System.setErr(oldStdErr);
         System.out.println(autoFight.getResult());
         System.out.println(autoFight.getLeftScore());
         System.out.println(autoFight.getRightScore());
         System.exit(0);
+    }
+
+    private static Paddle loadPaddle(String className) throws RegisterTimeoutException, GetTeamNameException, NewInstanceException, IOException, ClassNotFoundException {
+        Class<Paddle> paddleClass = (Class<Paddle>) Class.forName(className);
+        PaddleCache.registerNewPaddle(paddleClass);
+
+        return PaddleCache.getInstance(paddleClass);
+    }
+
+    /**
+     * We don't want any pesky System.out.print to mess with our pretty output format.
+     */
+    private static void redirectStdStreams() {
+        PrintStream stream = new PrintStream(new ByteArrayOutputStream());
+        System.setOut(stream);
+        System.setErr(stream);
     }
 }
--- README.md
+++ README.md
@@ -1,6 +1,50 @@
-robopong
+Robopong
 ========
 
 Like RoboCode, but with Pong instead of tanks.
 
 Requires Java 8 and JavaFX.
+
+## Introduction
+
+### Screenshots!
+
+### Goal
+
+### API
+
+## Ways to play
+
+### Local
+Compile and run the LANServer module on the server
+Compile and distribute the jar file from Client to everyone who wants to play.
+
+### Web
+Compile and run the WebServer module on the server
+Compile and distribute the jar file from Client to everyone who wants to play. The players then uploads their source file to the web server. There is no example HTML form for this yet.
+
+## Compiling
+These instructions where written using Gradle 2.2.1.
+
+### Compiling the client
+This requires JavaFX, and thus Oracle JDK 8
+
+From the git root
+```
+$ cd Client
+$ gradle jar
+$ java -jar build/libs/Client-1.0.jar
+```
+### Compiling the LAN server
+This requires JavaFX, and thus Oracle JDK 8
+
+From the git root
+```
+$ cd LANServer
+$ gradle jar
+$ java -jar build/libs/LANServer-1.0.jar
+```
+
+### Compiling the web server
+
+## Running
